f8cb1652ce81301f58e5faa7fbf8ce7fbfe1223f
Kendra Kyndberg <kendrak922@gmail.com>
2018-06-21 08:49:31 -0500

    heroku connection

---
+++ server.js
@@ -0,0 +1,61 @@
+
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const request = require('request');
+const cheerio = require('cheerio')
+
+//mongo
+const db = require("./models");
+
+//mongoose
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+const PORT = 3000;
+
+///express
+const app = express();
+app.use(express.static("public"));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+//cheerio
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get("/scrape", function(req, res){
+    request("https://vox.com",function(err, response ,html){
+        const $ = cheerio.load(response.data)
+        $(".title").each(function(i, element){
+            let result = {};
+            result.title = $(this).children("a").text();
+            result.link = $(this).childeren("a").attr("href");
+
+        db.Article.create(result)
+        .then(function(dbArticle){
+            console.log(dbArticle);
+        })
+        .catch(function(err){
+            return res.json(err);
+     });
+    });
+});
+    });
+    
+
+ 
+// $('h2.title').text('Hello there!')
+// $('h2').addClass('welcome')
+ 
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
